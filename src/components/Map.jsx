f8e6cc2435205e3592b5be398cfd62d995b855ca
hs286 <hamzakhalid4992@gmail.com>
2024-07-17 03:12:55 +0500

    map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,29 +1,74 @@
-'use client'
+'use client';
 import { Spinner } from '@nextui-org/react';
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api'
-import React, { useCallback, useState } from 'react'
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+import React, { useCallback, useState, useEffect } from 'react';
+
 const containerStyle = {
     width: '100%',
     height: "33vh"
-}
-// const center = {
-//     lat: 31.5217980444476,
-//     lng: 74.36008316527771,
-// };
+};
+
 const Map = ({ center }) => {
     const { isLoaded } = useJsApiLoader({
         id: 'google-map-script',
         googleMapsApiKey: 'AIzaSyDCQ_UrxKRUnMpP_f7mnT0BxZcjcSnp7EY'
-    })
+    });
     const [map, setMap] = useState(null);
+
     const onLoad = useCallback(function callback(map) {
-        const bounds = new window.google.maps.LatLngBounds(center)
-        map.fitBounds(bounds)
-        setMap(map)
-    }, [])
+        const bounds = new window.google.maps.LatLngBounds(center);
+        map.fitBounds(bounds);
+        setMap(map);
+    }, [center]);
+
     const onUnmount = useCallback(function callback(map) {
-        setMap(null)
-    }, [])
+        setMap(null);
+    }, []);
+
+    useEffect(() => {
+        if (isLoaded && map) {
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(
+                    (position) => {
+                        const pos = {
+                            lat: position.coords.latitude,
+                            lng: position.coords.longitude,
+                        };
+
+                        // Center the map on the user's location
+                        map.setCenter(pos);
+
+                        // Add a marker at the user's location
+                        new window.google.maps.Marker({
+                            position: pos,
+                            map: map,
+                            title: "You are here",
+                        });
+                    },
+                    () => {
+                        handleLocationError(true, map.getCenter());
+                    }
+                );
+            } else {
+                // Browser doesn't support Geolocation
+                handleLocationError(false, map.getCenter());
+            }
+        }
+    }, [isLoaded, map]);
+
+    function handleLocationError(browserHasGeolocation, pos) {
+        const infoWindow = new window.google.maps.InfoWindow({
+            position: pos,
+        });
+
+        infoWindow.setContent(
+            browserHasGeolocation
+                ? "Error: The Geolocation service failed."
+                : "Error: Your browser doesn't support geolocation."
+        );
+        infoWindow.open(map);
+    }
+
     return isLoaded ? (
         <GoogleMap
             mapContainerStyle={containerStyle}
@@ -31,16 +76,13 @@ const Map = ({ center }) => {
             zoom={8}
             onLoad={onLoad}
             onUnmount={onUnmount}
-
-
         >
         </GoogleMap>
     ) : (
-        <div className='flex items-center justify-center h-[30vh]'> 
-        <Spinner color="default" label='Map Loading' />
+        <div className='flex items-center justify-center h-[30vh]'>
+            <Spinner color="default" label='Map Loading' />
         </div>
     );
 };
 
-
-export default Map
+export default Map;
